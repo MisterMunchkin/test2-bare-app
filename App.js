204c95518ef144fc6f45b2b41f1c5899c55c6a40
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2022-02-17 00:53:32 +0800

    ios now works with expo-modules.

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
+  console.log(Constants.systemFonts);
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
