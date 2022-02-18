0445cbbb3a9525734d7e037437045640053e6845
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2022-02-18 23:47:27 +0800

    new tests

--- App.js
+++ App.js
@@ -2,9 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
+import * as HIDMobileLibrary from 'expo-hid-mobile-wrapper';
 
+//let HIDMobile = require('expo-hid-mobile-wrapper');
 export default function App() {
-  console.log(Constants.systemFonts);
+  //console.log(Constants.systemFonts);
+  
+  //console.log(HIDMobile.helloAsync());
+  HIDMobileLibrary.helloAsync(
+    {
+      'a': 'a'
+    }
+  )
+  .then(function (result) {
+    console.log(result);
+  });
+
+  HIDMobileLibrary.testAsync()
+  .then(function (result) {
+    console.log(result);
+  });
   
   return (
     <View style={styles.container}>
